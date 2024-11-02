f50d7623f3d6185277473c6e6dfd8f2b3dd97854
pawelorenc <pawellorenc@macbook-pro-pawe-1.home>
2024-11-02 18:27:37 +0100

    Druga wersja zadania

---
+++ zad7.2.py
@@ -0,0 +1,30 @@
+#Zadanie 7 - Napisz program, ktory oblicza wartosc N! dla N w zakresie od 1 do M, gdzie M jest wartoscia podana przez uzytkownika.
+
+def liczba(a) -> int:
+    while True:
+        try:
+            k = int(input(a))
+            if k < 1 or k > 1000000:
+                print("Podana zla wartosc!")
+                continue
+            return k
+        except ValueError:
+            print("Podana zla wartosc, podaj liczbe calkowita!")
+
+def silnia(k: int):
+    suma = 1
+    for i in range(1, k + 1):
+        suma *= i
+        roznica = int(suma / i)
+        print(suma)
+
+def roznica(q: int):
+    suma = 1
+    for i in range(2, q + 1): #Pomijamy pierwsza roznice poniewaz 1 - 1 = 0 nic nie wnosi :)
+        suma *= i
+        roznica = int(suma / i)
+        print(f"Dla {i} dzialania roznica wyniku to {suma}-{roznica}={suma-roznica}!")
+
+n = liczba("Podaj liczbe: ")
+silnia(n)
+roznica(n)
